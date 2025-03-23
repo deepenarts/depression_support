525645928bb8583fc8064bedfc78e5a6d587d0d7
Dipendra Kafle <dipendrakafle2000@gmail.com>
2025-03-23 11:44:47 +0545

    migrated database to postgresql in settings.py

--- depression/settings.py
+++ depression/settings.py
@@ -65,13 +65,17 @@ WSGI_APPLICATION = 'depression.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'depressionSupport',  
+        'USER': 'postgres',  
+        'PASSWORD': 'admin123',  
+        'HOST': 'depressionsupport.cfamoi26ogvs.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
