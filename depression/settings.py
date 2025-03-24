b108175a3b33addb1954619bc8d526075ce6a36a
Deepen Kafle <97222403+deepenarts@users.noreply.github.com>
2025-03-24 11:23:28 +0545

    Update settings.py

--- depression/settings.py
+++ depression/settings.py
@@ -63,25 +63,25 @@ TEMPLATES = [
 WSGI_APPLICATION = 'depression.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'depressionSupportDB',  
-#         'USER': 'postgres',  
-#         'PASSWORD': 'admin123',  
-#         'HOST': 'depressionsupport.cfamoi26ogvs.us-east-1.rds.amazonaws.com',
-#         'PORT': '5432',
-#     }
-# }
-
+Database
+https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'depressionSupportDB',  
+        'USER': 'postgres',  
+        'PASSWORD': 'admin123',  
+        'HOST': 'depressionsupport.cfamoi26ogvs.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
